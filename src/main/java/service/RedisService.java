4ecb5637403e2759f58f3f0b6ff78038bc50a0d3
khushwantTTN <khushwant@tothenew.com>
2023-05-23 12:17:52 +0530

    created Redis Service and Service Impl

---
+++ src/main/java/service/RedisService.java
@@ -0,0 +1,8 @@
+package service;
+
+public interface RedisService {
+
+    void set(String key, Object value);
+
+    Object get(String key);
+}
---
+++ src/main/java/service/RedisServiceImpl.java
@@ -0,0 +1,24 @@
+package service;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisServiceImpl implements RedisService {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public RedisServiceImpl(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public void set(String key, Object value) {
+
+    }
+
+    @Override
+    public Object get(String key) {
+        return null;
+    }
+}
