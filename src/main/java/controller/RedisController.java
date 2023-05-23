c9ad375d7fc12b2f5a5e956221c2e0e9f31c4eea
khushwantTTN <khushwant@tothenew.com>
2023-05-23 12:20:56 +0530

    implemented controller

---
+++ src/main/java/controller/RedisController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import service.RedisService;
+
+@RestController
+@RequestMapping("/redis")
+@RequiredArgsConstructor
+public class RedisController {
+
+    @Autowired
+    RedisService redisService;
+
+    @GetMapping(path = "/set")
+    public void set(
+            @RequestParam(name = "key") String key,
+            @RequestParam(name = "value") String value
+    ) {
+        redisService.set(key, value);
+    }
+
+    @GetMapping(path = "/get")
+    public String get(@RequestParam(name = "key") String key) {
+        return (String) redisService.get(key);
+    }
+
+}
